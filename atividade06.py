6079a7d18119be4d6965a3d323dd9521826c3e58
kawehenri <kawehenri@gmail.com>
2024-08-20 11:49:16 -0300

    desafio_codigo.py

---
+++ atividade06.py
@@ -0,0 +1,8 @@
+nota1 = int(input("Insira sua nota na prova 1: "))
+nota2 = int(input("Insira sua nota na prova 2: "))
+nota3 = int(input("Insira sua nota na prova 3: "))
+nota4 = int(input("Insira sua nota na prova 4: "))
+nota5 = int(input("Insira sua nota na prova 5: "))
+
+media = (nota1 + nota2 + nota3 + nota4 + nota5) /5
+print(f"Sua media de nota nas provas foi: {media}")
---
+++ desafio_codigo.py
@@ -0,0 +1,5 @@
+num1 = int(input("Insira um número: "))
+num2 = int(input("Insira outro número: "))
+soma = num1 + num2
+print("A soma dos números é: ", soma)
+
